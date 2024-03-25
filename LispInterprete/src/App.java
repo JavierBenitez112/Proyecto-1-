95d238bd81caac00a5f259652757d7110a1e965a
Brariv <119123309+Brariv@users.noreply.github.com>
2024-03-24 20:44:14 -0600

    update

--- LispInterprete/src/App.java
+++ LispInterprete/src/App.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-import Analizador.AST;
-import Evaluador.Evaluador;
+
 import Tokens.LispReader;
 import Tokens.tokenizer;
-import Analizador.Node;
+
 import Analizador.parser;
 
 public class App {
@@ -12,42 +12,28 @@ public class App {
     // Metodo main que corre el programa
     public static void main(String[] args) throws Exception {
         LispReader lisp = new LispReader();
-        ArrayList<String> tokens = lisp.LispFile("C:\\Users\\javib\\OneDrive\\Documentos\\GitHub\\Proyecto-1-\\LispInterprete\\Farenheit.lsp");
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("ingrese la direccion del archivo lisp:");
+        String path = sc.nextLine();
+        
+        path = path.replaceAll("\"", "");
+
+
+        ArrayList<String> tokens = lisp.LispFile(path);
         //System.out.println(tokens);
         tokenizer token = new tokenizer();
         token.tokenize(tokens); // Fix: Pass the 'tokens' ArrayList as an argument
         System.out.println(token.getTokenMap());
         System.out.println(token.getTokened());
-
-                //Prueba Evaluador defun
-                // Construir el árbol
-                Node<String> n1 = new Node<>("2");
-                Node<String> n2 = new Node<>("3");
-                Node<String> n3 = new Node<>("4");
-                Node<String> n4 = new Node<>("5");
-                Node<String> n5 = new Node<>("+");
-                Node<String> n6 = new Node<>("-");
-                Node<String> n7 = new Node<>("-");
         
-                n5.addHijo(n1);
-                n5.addHijo(n2);
-                n6.addHijo(n3);
-                n6.addHijo(n4);
-                n7.addHijo(n5);
-                n7.addHijo(n6);
-        
-                AST<String> tree = new AST<>(n7);
-        
-                // Probar el evaluador
-                Evaluador evaluator = new Evaluador();
-                String resultado = evaluator.evaluate(tree);
-                System.out.println("El resultado de la expresión es: " + resultado);
 
-                //Prueba parser
-                parser p = new parser(token.getTokenMap());
-                p.parse(tokens);
+        //Prueba parser
+        parser p = new parser(token.getTokenMap());
+        p.parse(tokens);
 
-                p.printCurrentAST();
+        p.printCurrentAST();
     }
 
 
